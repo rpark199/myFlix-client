1ab40194ae32f8b33ac50648cfc7a17a4e0c5f78
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-06-01 18:28:48 -0400

    imported proptypes and added proptypes to moviecard

--- src/Components/movie-card/movie-card.jsx
+++ src/Components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
       <div
@@ -8,4 +10,13 @@ export const MovieCard = ({ movie, onMovieClick }) => {
         {movie.Title}
       </div>
     );
+  };
+
+  MovieCard.propTypes = {
+    movie: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      author:PropTypes.string
+    }).isRequired,
+    onBookClick: PropTypes.func.isRequired
   };
