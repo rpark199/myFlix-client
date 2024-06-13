e9df79b543c8965df8d0c76d5b140456e5f27ae5
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-06-13 12:08:51 -0400

    Added useState, updated const LoginView

--- src/Components/login-view/login-view.jsx
+++ src/Components/login-view/login-view.jsx
@@ -1,13 +1,57 @@
-export const LoginView = () => {
+import { useState } from "react";
+
+export const LoginView = ({ onLoggedIn }) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = {
+            access: username,
+            secret: password
+        };
+
+        fetch("https://movie-api-dwho.onrender.com/account/login.json", {
+            method: "POST",
+            headers: { Authorization: `Bearer ${token}` },
+            body: JSON.stringify(data)
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("Login response: ", data);
+                if (data.user) {
+                    localStorage.setItem("user", JSON.stringify(data.user));
+                    localStorage.setItem("token", data.token);
+                    onLoggedIn(data.user, data.token);
+                } else {
+                    alert("No such user");
+                }
+            })
+            .catch((e) => {
+                alert("Something went wrong");
+            });
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
                 Username:
-                <input type="text" />
+                <input 
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)} 
+                    required
+                />
             </label>
             <label>
                 Password:
-                <input type="password" />
+                <input 
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
             </label>
             <button type="submit">Submit</button>
         </form>
