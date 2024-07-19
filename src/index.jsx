1219e4c2118f97d05608348d6f1f4da7819495f9
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-07-19 16:02:10 -0400

    Add store and redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,26 @@
 import { createRoot } from "react-dom/client";
+
 import { MainView } from "./Components/main-view/main-view";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./index.scss";
 import { Container } from "react-bootstrap";
+
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
 
 const MyFlixApplication = () => {
   return (
-<Container>
-     <MainView />
-     </Container>
-
+    <Provider store={store}>
+      <Container>
+        <MainView />
+      </Container>
+    </Provider>
   );
 };
 
-// Finds the root of your app
 const container = document.querySelector("#root");
 const root = createRoot(container);
 
-// Tells React to render your app in the root DOM element
 root.render(<MyFlixApplication />);
