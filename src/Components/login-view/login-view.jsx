8885c979160ff52b42abdcba63dfe554c13d74cf
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-07-19 15:54:52 -0400

    Add container, row, and col bootstrap

--- src/Components/login-view/login-view.jsx
+++ src/Components/login-view/login-view.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { Container, Row, Col } from "react-bootstrap";
 
 export const LoginView = ({ onLoggedIn }) => {
     const [username, setUsername] = useState("");
@@ -23,7 +24,6 @@ export const LoginView = ({ onLoggedIn }) => {
         })
             .then((response) => response.json())
             .then((data) => {
-                console.log("Login response: ", data);
                 if (data.user) {
                     localStorage.setItem("user", JSON.stringify(data.user));
                     localStorage.setItem("token", data.token);
@@ -38,28 +38,35 @@ export const LoginView = ({ onLoggedIn }) => {
     };
 
     return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formUsername">
-                <Form.Label>Username:</Form.Label>
-                <Form.Control
+        <Container className="login-section mb-4 p-4">
+          <Row className="justify-content-center">
+            <Col xs={12}>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="formUsername">
+                  <Form.Label>Username:</Form.Label>
+                  <Form.Control
                     type="text"
                     value={username}
-                    onChange={(e) => setUsername(e.target.value)} 
+                    onChange={(e) => setUsername(e.target.value)}
                     required
                     minLength="3"
-                />
-            </Form.Group>
-            
-            <Form.Group controlId="formPassword">
-                <Form.Label>Password:</Form.Label>
-                <Form.Control
+                  />
+                </Form.Group>
+                <Form.Group controlId="formPassword">
+                  <Form.Label>Password:</Form.Label>
+                  <Form.Control
                     type="password"
                     value={password}
-                    onChange={(e) => setPassword(e.target.value)} 
+                    onChange={(e) => setPassword(e.target.value)}
                     required
-                />
-            </Form.Group>
-            <Button variant="primary" type="submit">Submit</Button>
-        </Form>
-    );
-};
+                  />
+                </Form.Group>
+                <Button variant="primary" type="submit" className="login-button">
+                  Submit
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      );
+    };
