92055ddac1ebdd8bd586e96f5e4724821c848e0c
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-07-19 15:59:31 -0400

    created profile del, update, user info and fav

---
+++ src/Components/profile-view/profile-update.jsx
@@ -0,0 +1,170 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Form, Button, Col } from "react-bootstrap";
+import moment from "moment";
+
+export const ProfileUpdate = ({ username, token, user, onProfileUpdate }) => {
+  const formatDate = (dateString) => {
+    let utcDate = moment.utc(dateString);
+
+    let formattedDate = utcDate.format("YYYY-MM-DD");
+
+    return formattedDate;
+  };
+
+  const initialFormData = {
+    Username: user.Username || "",
+    Password: "",
+    ConfirmPassword: "",
+    Email: user.Email || "",
+    Birthday: user.Birthday ? formatDate(user.Birthday) : "",
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [passwordError, setPasswordError] = useState(null);
+
+  useEffect(() => {
+    setFormData({
+      Username: user.Username || "",
+      Password: "",
+      ConfirmPassword: "",
+      Email: user.Email || "",
+      Birthday: user.Birthday ? formatDate(user.Birthday) : "",
+    });
+  }, [user]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+
+    if (name === "Password" || name === "ConfirmPassword") {
+      if (
+        (name === "Password" && value !== formData.ConfirmPassword) ||
+        (name === "ConfirmPassword" && value !== formData.Password)
+      ) {
+        setPasswordError("Passwords do not match.");
+      } else {
+        setPasswordError(null);
+      }
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Check if passwords match
+    if (formData.Password !== formData.ConfirmPassword) {
+      setPasswordError("Passwords do not match.");
+      return;
+    }
+
+    // Check if password meets minimum length requirement
+    if (formData.Password.length > 0 && formData.Password.length < 8) {
+      setPasswordError("Password must be at least 8 characters long.");
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const response = await axios.put(
+        `"https://moviflex-a914bff79426.herokuapp.com/users/${username}`,
+        {
+          ...formData,
+          Birthday: formatDate(formData.Birthday),
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const updatedUser = response.data;
+      localStorage.setItem("user", JSON.stringify(updatedUser));
+      onProfileUpdate(updatedUser);
+      alert("Profile updated successfully");
+      // Force window reload after successful update
+      window.location.reload();
+    } catch (error) {
+      setError(error.message);
+      alert("Failed to update profile");
+      // Force window reload after failed update
+      window.location.reload();
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Col md={8} className="profile-update">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            type="text"
+            name="Username"
+            value={formData.Username}
+            onChange={handleChange}
+            required
+            minLength="3"
+          />
+        </Form.Group>
+        <Form.Group controlId="formPassword">
+          <Form.Label>New Password:</Form.Label>
+          <Form.Control
+            type="password"
+            name="Password"
+            value={formData.Password}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="formConfirmPassword">
+          <Form.Label>Confirm New Password:</Form.Label>
+          <Form.Control
+            type="password"
+            name="ConfirmPassword"
+            value={formData.ConfirmPassword}
+            onChange={handleChange}
+            isInvalid={!!passwordError}
+          />
+          <Form.Control.Feedback type="invalid">
+            {passwordError}
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group controlId="formEmail">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control
+            type="email"
+            name="Email"
+            value={formData.Email}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="formBirthday">
+          <Form.Label>Birthday:</Form.Label>
+          <Form.Control
+            type="date"
+            name="Birthday"
+            value={formData.Birthday}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+        <Button
+          variant="primary"
+          type="submit"
+          disabled={loading}
+          className="update-button"
+        >
+          {loading ? "Updating..." : "Update"}
+        </Button>
+        {error && <div className="text-danger mt-3">Error: {error}</div>}
+      </Form>
+    </Col>
+  );
+};
--- src/Components/profile-view/profile-view.jsx
+++ src/Components/profile-view/profile-view.jsx
@@ -1,98 +1,136 @@
-import { useState } from "react";
-import { Button, Card, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { MovieCard } from "../movie-card/movie-card";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
+import { Row, Col } from "react-bootstrap";
+import { UserInfo } from "./user-info";
+import { ProfileUpdate } from "./profile-update";
+import { ProfileDelete } from "./profile-delete";
+import { FavoriteMovies } from "./favorite-movies";
+import { useNavigate } from "react-router-dom";
+import moment from "moment";
 
-export const ProfileView = ({ movies }) => {
-    const localUser = JSON.parse(localStorage.getItem("user"));
-    const fav = movies.filter((movie) => {
-        return localUser.FavoriteMovies.includes(movie.id);
-    });
+export const ProfileView = ({ username, token, onLogout, movies }) => {
+    const [user, setUser] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const navigate = useNavigate();
 
-    const [username, setUsername] = useState(localUser.Username||"");
-    const [password, setPassword] = useState(localUser.Password||"");
-    const [email, setEmail] = useState(localUser.Email||"");
-    const [birthday, setBirthday] = useState(localUser.Birthday||"01/01/0001");
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        const data = {
-            Username: username,
-            Password: password,
-            Email: email,
-            Birthday: birthday
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const response = await axios.get(
+                    "https://moviflex-a914bff79426.herokuapp.com/users/${username}",
+                    {
+                        headers: {
+                            Authorization:`Bearer ${token}`,
+                        }
+                    }
+                );
+                setUser(response.data);
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
         };
 
-        fetch("https://moviflex-a914bff79426.herokuapp.com/users", {
-            method: "POST",
-            body: JSON.stringify(data),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }).then((response) => {
-            if (response.ok) {
-              alert("Signup successful");
-              window.location.reload();
-            } else {
-              alert("Signup failed");
-            }
-          });
-        };
-        console.log(fav);
-        return (
-            <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formUsername">
-            <Form.Label>
-                Username:
-            </Form.Label>
-            <Form.Control
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-                minLength='4'
-                /> 
-            </Form.Group>
-            <Form.Group controlId="formPassword">
-            <Form.Label>
-                Password:
-            </Form.Label>
-                <Form.Control
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                />
-            </Form.Group>
-            <Form.Group controlId="formEmail">
-            <Form.Label>
-                Email:
-            </Form.Label>
-                <Form.Control
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                />
-            </Form.Group>
-            <Form.Group controlId="formBdate">
-            <Form.Label>
-                Birthday:
-            </Form.Label>
-                <Form.Control
-                type="date"
-                value={birthday}
-                onChange={(e) => setBirthday(e.target.value)}
-                required
-                />
-            </Form.Group>
-            <Button variant="primary" type="submit">Edit Profile</Button>
-            {
-            localUser && fav.map((movie) => (
-                <MovieCard movie={movie}>
-                </MovieCard>
-                ))}
-            </Form>
-        );
+    fetchUserData();
+}, [username, token]);
+
+    const handleUpdate = (updatedUser) => {
+    setUser(updatedUser); 
+    navigate(`/users/${updatedUser.Username}`, { replace: true });
     };
+
+    const handleDelete = async () => {
+        if (window.confirm("Are you sure you want to delete your profile?")) {
+        setIsDeleting(true);
+        try {
+            const response = await axios.delete(
+              `https://moviflex-a914bff79426.herokuapp.com/users/${username}`,
+              {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              }
+            );
+            if (response.status === 200) {
+              alert("Profile deleted");
+              onLogout();
+              navigate("/login");
+            } else {
+              alert("Failed to delete profile:", response.statusText);
+              setIsDeleting(false);
+            }
+          } catch (error) {
+              console.error("Error deleting profile:", error);
+          }
+        }
+    };
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>
+    }
+
+    const favoriteMovies = movies.filter((m) =>
+        user.FavoriteMovies.includes(m._id)
+    );
+
+    const formattedBirthday = moment.utc(user.Birthday).format("MM-DD-YYYY");
+    
+    return (
+        <Row className="justify-content-center">
+          <Col lg={8}>
+            <div className="profile-section mb-4 p-4">
+              <h1 className="text-center mb-4">Profile Info</h1>
+              <UserInfo
+                username={user.Username}
+                email={user.Email}
+                birthday={formattedBirthday}
+              />
+            </div>
+            <div className="profile-section mb-4 p-4">
+              <h2 className="text-center mb-4">Update Info</h2>
+              <ProfileUpdate
+                username={username}
+                token={token}
+                user={user}
+                onProfileUpdate={handleUpdate}
+              />
+              <ProfileDelete username={username} onDelete={handleDelete} />
+            </div>
+          </Col>
+          <Row className="mb-4">
+            <Col xs={12}>
+              <div className="profile-section p-4">
+                <h2 className="text-center mb-4">Favorite Movies</h2>
+                <FavoriteMovies
+                  favoriteMovies={favoriteMovies}
+                  user={user}
+                  token={token}
+                  onUpdateFavorites={(movieId) => {
+                    setUser((prevUser) => ({
+                      ...prevUser,
+                      FavoriteMovies: prevUser.FavoriteMovies.filter(
+                        (id) => id !== movieId
+                      ),
+                    }));
+                  }}
+                />
+              </div>
+            </Col>
+          </Row>
+        </Row>
+      );
+    };
+
+ProfileView.propTypes = {
+    username: PropTypes.string.isRequired,
+    token: PropTypes.string.isRequired,
+    onLogout: PropTypes.func.isRequired,
+    movies: PropTypes.array.isRequired,
+  };
